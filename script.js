72870a251fcf52aebd68b5f47ffffe41696b5b40
7freez7 <filaa.07cz@gmail.com>
2025-11-10 16:22:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, url) {
+  const response = await fetch(url);
+  const html = await response.text();
+  document.getElementById(id).innerHTML = html;
+}
+loadComponent('header', 'components/header.html');
+loadComponent('footer', 'components/footer.html');
+loadComponent('sluzby', 'components/sluzby.html');
+loadComponent('kontakt', 'components/kontakt.html');
