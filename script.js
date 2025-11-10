fe8da77ab06565b01bcc754258c756c3f68ec8b6
7freez7 <filaa.07cz@gmail.com>
2025-11-10 23:42:56 +0100

    about, vetsina vseho

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ async function loadComponent(id, url) {
 }
 loadComponent('header', 'components/header.html');
 loadComponent('footer', 'components/footer.html');
+
 loadComponent('sluzby', 'components/sluzby.html');
 loadComponent('kontakt', 'components/kontakt.html');
+
+loadComponent('partneri', 'role/partneri.html');
+loadComponent('advokati', 'role/advokati.html');
+loadComponent('koncipienti', 'role/koncipienti.html');
+loadComponent('administrativa', 'role/administrativa.html');
